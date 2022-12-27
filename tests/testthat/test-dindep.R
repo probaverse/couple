21bd1360146d078afe60f7401bf74dcbc8421339
Vincenzo Coia <vincenzo.coia@gmail.com>
2022-12-26 23:22:57 -0800

    combine Mariia's tests

--- tests/testthat/test-dindep.R
+++
@@ -1,8 +0,0 @@
-test_that("dindep works", {
-  expect_equal(dindep(c(0.3, 2), c(0.2, 0.5)), c(1, 0))
-  expect_equal(dindep(c(NA, 2), c(0.2, 0.5)), c(NA, 0))
-  expect_equal(dindep(c(0.3, 2, 0.4), c(0.2)), c(1, 0, 1))
-  expect_equal(dindep(c(0.3), c()), numeric(0))
-  expect_error(dindep(c(0.3, 2), c(0.2, 0.5, 0.4)))
-  expect_error(dindep(c(0.3, NA), c(0.2, 0.5, 0.4)))
-})
