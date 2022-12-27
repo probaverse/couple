70e4358b2381ef79f7c5fab4a6bece1630e8683d
Vincenzo Coia <vincenzo.coia@gmail.com>
2022-12-26 22:34:31 -0800

    Minor changes to dindep()

--- R/dindep.R
+++ R/dindep.R
@@ -1,18 +1,15 @@
-#' @title density of the independence copula
+#' Density of the Independence Copula
 #'
-#' @description identifies the density of the independence copula,
+#' Evaluates the density of the independence copula,
 #' 1 everywhere on the unit square, and 0 elsewhere
 #'
-#' @param u A numeric vector
-#'
-#' @param v A numeric vector
-#' @return A numeric vector
+#' @param u,v Numeric vectors.
+#' @return A numeric vector.
 #' @examples
 #' dindep(c(1, 0.3, 5, 0.8), c(0.5, 0.7, 3, 8))
-#' dindep(c(0.2, 0.3), c(4))
+#' dindep(c(0.2, 0.3), 4)
 #'
 #' @export
-
 dindep <- function(u, v) {
   if ((length(u) == 0) | (length(v) == 0)) {
     return(numeric(0))
@@ -20,7 +17,6 @@ dindep <- function(u, v) {
   vectors_update <- vctrs::vec_recycle_common(u, v)
   u <- vectors_update[[1]]
   v <- vectors_update[[2]]
-  density <- c(as.integer((0 <= u) & (u <= 1) & (0 <= v) & (v <= 1)))
-  return(density)
+  as.numeric((0 <= u) & (u <= 1) & (0 <= v) & (v <= 1))
 }
 
