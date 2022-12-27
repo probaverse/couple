23245894295bb571b3aa2f55984ef2e67b17df10
Mariia Morello <mmorello@BGC2918.local>
2022-12-26 20:19:48 -0800

    Functions code revision

--- R/dindep.R
+++ R/dindep.R
@@ -1,32 +1,26 @@
-#' density of the independence copula
+#' @title density of the independence copula
 #'
-#' identifies the density of the independence copula,
+#' @description identifies the density of the independence copula,
 #' 1 everywhere on the unit square, and 0 elsewhere
 #'
-#' @param vector_1 A numeric vector
+#' @param u A numeric vector
 #'
-#' @param vector_2 A numeric vector
+#' @param v A numeric vector
 #' @return A numeric vector
 #' @examples
 #' dindep(c(1, 0.3, 5, 0.8), c(0.5, 0.7, 3, 8))
+#' dindep(c(0.2, 0.3), c(4))
 #'
 #' @export
 
-library(dplyr)
-dindep <- function(vector_1, vector_2) {
-
-  if ((length(vector_1) == 1) | (length(vector_2) == 1)) {
-    vector_1 <- vctrs::vec_recycle_common(vector_1, vector_2)[[1]]
-    vector_2 <- vctrs::vec_recycle_common(vector_1, vector_2)[[2]]
+dindep <- function(u, v) {
+  if ((length(u) == 0) | (length(v) == 0)) {
+    return(numeric(0))
   }
-  if ((length(vector_1) == 0) | (length(vector_2) == 0)) {
-    return(0)
-  }
-
-  if (length(vector_1) != length(vector_2)) {
-    stop("Unable to operate on the vectors of a different length if none of them is of length 1 ")
-  }
-  output_vector <- between(vector_1, 0, 1) & between(vector_2, 0, 1)
-  ifelse(is.na(output_vector), NA, as.integer(output_vector))
+  vectors_update <- vctrs::vec_recycle_common(u, v)
+  u <- vectors_update[[1]]
+  v <- vectors_update[[2]]
+  density <- c(as.integer((0 <= u) & (u <= 1) & (0 <= v) & (v <= 1)))
+  return(density)
 }
 
--- R/pindep.R
+++ R/pindep.R
@@ -1,15 +1,15 @@
 #' @title Cdf of the independence copula
 #'
 #' @description Identifies the cdf of the independence copula:
-#' vector_1 * vector_2 - if both vectors are located on the unit square;
+#' u * v - if both vectors are located on the unit square;
 #' 1 - if both vectors are larger than 1;
 #' 0 - if one of the vectors is below 0;
-#' vector_1 value - if vector_2 is larger than 1;
-#' vector_2 value - if vector_1 is larger than 1
+#' u value - if v is larger than 1;
+#' v value - if u is larger than 1
 #'
-#' @param vector_1 A numeric vector
+#' @param u A numeric vector
 #'
-#' @param vector_2 A numeric vector
+#' @param v A numeric vector
 #' @return A numeric vector
 #' @examples pindep(c(1, 0.2), c(0.5))
 #' pindep(c(-2, 8, NA), c(3, 4, 0.1))
@@ -17,34 +17,29 @@
 #'
 #' @export
 
-pindep <- function(vector_1, vector_2) {
+pindep <- function(u, v) {
 
-  if ((length(vector_1) == 1) | (length(vector_2) == 1)) {
-    vector_1 <- vctrs::vec_recycle_common(vector_1, vector_2)[[1]]
-    vector_2 <- vctrs::vec_recycle_common(vector_1, vector_2)[[2]]
-  }
-  if ((length(vector_1) == 0) | (length(vector_2) == 0)) {
+  if ((length(u) == 0) | (length(v) == 0)) {
     return(0)
   }
+  vectors_update <- vctrs::vec_recycle_common(u, v)
+  u <- vectors_update[[1]]
+  v <- vectors_update[[2]]
 
-  if (length(vector_1) != length(vector_2)) {
-    stop("Unable to operate on the vectors of a different length if none of them is of length 1 ")
-  }
-  bool_vector_unit_square <- between(vector_1, 0, 1) & between(vector_2, 0, 1)
-  final_vector_unit_square <- ifelse(bool_vector_unit_square == TRUE, vector_1 * vector_2, FALSE)
+  bool_vector_unit_square <- (0 <= u) & (u <= 1) & (0 <= v) & (v <= 1)
+  final_vector_unit_square <- ifelse(bool_vector_unit_square == TRUE, u * v, bool_vector_unit_square)
 
-  bool_vector_larger_than_1 <- (vector_1 > 1) & (vector_2 > 1) & (final_vector_unit_square == FALSE)
+  bool_vector_larger_than_1 <- (u > 1) & (v > 1) & (final_vector_unit_square == FALSE)
   final_both_vectors_larger_than_1 <- ifelse(bool_vector_larger_than_1 == TRUE, 1, final_vector_unit_square)
 
-  bool_vector_negative <- (vector_1 < 0) | (vector_2 < 0) & (final_both_vectors_larger_than_1 == FALSE)
+  bool_vector_negative <- (u < 0) | (v < 0) & (final_both_vectors_larger_than_1 == FALSE)
   final_vector_negative <- ifelse(bool_vector_negative == TRUE, 0, final_both_vectors_larger_than_1)
 
-  bool_vector_1_larger_than_1 <- (vector_1 > 1) & (final_vector_unit_square == FALSE) & (bool_vector_larger_than_1 == FALSE)
-  final_vector_1_larger_than_1 <- ifelse(bool_vector_1_larger_than_1 == TRUE, vector_2, final_vector_negative)
+  bool_u_larger_than_1 <- (u > 1) & (final_vector_unit_square == FALSE) & (bool_vector_larger_than_1 == FALSE)
+  final_u_larger_than_1 <- ifelse(bool_u_larger_than_1 == TRUE, v, final_vector_negative)
 
-  bool_vector_2_larger_than_1 <- (vector_2 > 1) & (final_vector_1_larger_than_1 == FALSE) & (bool_vector_larger_than_1 == FALSE)
-  final_vector_2_larger_than_1 <- ifelse(bool_vector_2_larger_than_1 == TRUE, vector_1, final_vector_1_larger_than_1)
+  bool_v_larger_than_1 <- (v > 1) & (final_u_larger_than_1 == FALSE) & (bool_vector_larger_than_1 == FALSE)
+  final_v_larger_than_1 <- ifelse(bool_v_larger_than_1 == TRUE, u, final_u_larger_than_1)
 
-  final_vector_2_larger_than_1
-
+  final_v_larger_than_1
 }
