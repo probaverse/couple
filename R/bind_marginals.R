fb2df48a363c9dbf60bdcadb870d7cae069610a0
Mariia Morello <mmorello@BGC2918.local>
2023-03-02 16:31:57 -0800

    Tests added

--- R/bind_marginals.R
+++ R/bind_marginals.R
@@ -9,11 +9,11 @@
 #' The list belongs to cop_dst class, which is a class of bi_dst, which is a class of mult_dst.
 #' @examples
 #' bind_marginals(cop_ig(theta = 1, alpha = 3), mtcars$hp, mtcars$disp)
-#' bind_marginals(copula_object, rnorm(50, 2), rnorm(50, 15))
+#' bind_marginals(cop_ig(theta = 1, alpha=2), rnorm(50, 2), rnorm(50, 15))
 #'
 #' @export
 bind_marginals <- function(copula, dst_x, dst_y){
   bivariate_dst <- list(copula, dst_x, dst_y)
   names(bivariate_dst) <- c("copula", "dst_x", "dst_y")
-  structure(bivariate_dst, class = c('cop_dst', 'bi_dst', 'mult_dst'))
+  structure(bivariate_dst, class = c('cop_dst', 'bi_dst', 'multi_dst'))
 }
