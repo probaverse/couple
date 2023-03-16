f86e5196a20b794b5142c5f71f8e20d34842572e
Vincenzo Coia <vincenzo.coia@gmail.com>
2023-03-16 09:49:51 -0700

    Fix bind_marginals list

--- R/bind_marginals.R
+++ R/bind_marginals.R
@@ -13,6 +13,6 @@
 #'
 #' @export
 bind_marginals <- function(copula, dst_x, dst_y){
-  bivariate_dst <- list(copula = 'copula', dst_x = 'dst_x', dst_y = 'dst_y')
+  bivariate_dst <- list(copula = copula, dst_x = dst_x, dst_y = dst_y)
   structure(bivariate_dst, class = c('cop_dst', 'bi_dst', 'multi_dst'))
 }
